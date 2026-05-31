c5402e6b8cdbfeaeaab7960ee53a0b478318abc9
Chandesh Mehta <mehtachandesh@gmail.com>
2026-05-31 17:56:21 +0530

    fix: inject SpeechService in AppComponent to initialize theme on landing/login/register pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SpeechService } from './services/speech/speech.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { RouterModule } from '@angular/router';
   template: `<router-outlet></router-outlet>`,
   styleUrls: ['./app.scss'],
 })
-export class App {}
+export class App {
+  private speechService = inject(SpeechService);
+}
